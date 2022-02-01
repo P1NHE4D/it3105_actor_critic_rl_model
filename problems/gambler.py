7a8a38215ddf40643ce3234c914a9977be48c543
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-01 16:31:02 +0100

    feat: add gambler problem

--- problems/gambler.py
+++ problems/gambler.py
@@ -1,19 +1,59 @@
-from rl.env import Domain
+from dataclasses import dataclass
+import numpy as np
+from rl.environment import Domain
+import matplotlib.pyplot as plt
+
+
+@dataclass(frozen=True)
+class GamblerState:
+    units: int
+
+    def get_actions(self):
+        return np.arange(start=1, stop=min(self.units, 100 - self.units) + 1)
 
 
 class Gambler(Domain):
 
-    def get_init_state(self):
-        pass
+    def __init__(self, config):
+        self.win_prob = config.get("win_prob", 0.5)
+        self.states = []
+        self.episode_count = 0
+        self.state_count = []
 
-    def get_child_state(self, action):
-        pass
+    def produce_initial_state(self):
+        self.states = []
+        self.episode_count += 1
+        units = np.random.random_integers(1, 99)
+        state = GamblerState(units=units)
+        self.states.append(state)
+        actions = state.get_actions()
+        return state.__hash__(), actions
+
+    def generate_child_state(self, action):
+        outcome = np.random.choice([-action, action], p=[1-self.win_prob, self.win_prob])
+        units = self.states[-1].units + outcome
+        state = GamblerState(units=units)
+        self.states.append(state)
+        actions = state.get_actions()
+
+        if units <= 0:
+            reinforcement = -100
+        elif units >= self.states[-2].units:
+            reinforcement = 1
+        else:
+            reinforcement = -1
+
+        return state.__hash__(), actions, reinforcement
 
     def is_current_state_terminal(self):
-        pass
+        units = self.states[-1].units
+        a = (units <= 0)
+        b = (units >= 100)
+        if a or b:
+            self.state_count.append(len(self.states))
 
-    def get_actions(self, state_id):
-        pass
+        return a or b
 
     def visualise(self):
+        # TODO
         pass
