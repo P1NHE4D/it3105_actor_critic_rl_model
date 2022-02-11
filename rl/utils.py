37015bff28a65a62a27847f0622369882e2a0635
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-02-11 09:38:33 +0100

    fix: update requriements.txt

--- rl/utils.py
+++ rl/utils.py
@@ -1,9 +1,6 @@
-from collections import UserDict, Callable
-
-
 class DefaultValueTable(dict):
 
-    def __init__(self, val_func: Callable, **kwargs):
+    def __init__(self, val_func, **kwargs):
         super().__init__(**kwargs)
         self.val_func = val_func
 
